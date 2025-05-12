6d7a1831478c118822952ba424d737015a355339
adelalu <adelaluwahab@gmail.com>
2025-05-12 20:08:07 +0100

    Initial commit

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -10,17 +10,20 @@ const Signup = () => {
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
   return (
-    <section className="flex flex-col justify-center items-center px-11 pb-11 pt-3">
-      <div className=" w-[20%] rounded-2xl ml-50 lg:flex hidden lg:mb-5 lg:flex md:flex sm:hidden">
-        <img src={logo2} alt="" className="h-full w-full " />
-      </div>
+    <section className="flex flex-col justify-center items-center px-11 pb-11 pt-10">
       <div className="grid lg:grid-cols-2  md:grid-cols-2 sm:grid-cols-1 items-center  lg:gap-0.5 md:gap-10">
         <div className="lg:flex md:flex  hidden justify-center items-center lg:w-[100%] md:w-[110%]">
           <img src={sign} alt="" className="h-full w-full rounded-2xl" />
         </div>
 
-        <div className="flex flex-col items-center justify-center ">
-          <div className="flex items-center py-10 lg:-ml-20 -ml-0 md:py-0 sm:bg-stone-100 bg-stone-100 md:bg-transparent lg:bg-transparent rounded-2xl flex-col min-w-sm lg:-mt-20">
+        <div className="flex flex-col items-center justify-center pt-10 ">
+          <div className="flex items-center py-10  md:py-0 sm:bg-stone-100 bg-stone-100 md:bg-transparent lg:bg-transparent rounded-2xl flex-col min-w-sm lg:-mt-20">
+            <div className="w-full flex justify-start">
+              <div className="w-[40%] rounded-2xl lg:flex md:flex sm:hidden hidden">
+                <img src={logo2} alt="" className="h-full w-full" />
+              </div>
+            </div>
+
             <div className="space-y-2 w-full">
               <div className="space-y-0.5 pl-7">
                 <div className="grid grid-cols-2 sm:grid-cols-2 md:flex md:items-center lg:flex lg:items-center justify-between">
@@ -33,7 +36,7 @@ const Signup = () => {
                   </div>
 
                   {/* Logo Section - only shows on mobile */}
-                  <div className="w-[40%] h-[40%] justify-self-end rounded-2xl md:hidden lg:hidden">
+                  <div className="w-[80%] h-[80%] justify-self-end rounded-2xl md:hidden lg:hidden">
                     <img
                       src={logo2}
                       alt="Company Logo"
@@ -117,7 +120,7 @@ const Signup = () => {
                       <input
                         type="tel"
                         id="phone"
-                        placeholder="Enter phone number"
+                        placeholder="Mobile number"
                         required
                         className="w-full sm:text-sm border-2 py-1.5 border-gray-200 rounded-md bg-gray-200 focus:outline-none focus:ring-0 pl-2"
                       />
@@ -161,7 +164,7 @@ const Signup = () => {
                   </span>
                 </div>
 
-                <button className="bg-black w-full text-white py-1.5 px-4 rounded-md mt-5 signup-btn">
+                <button className="bg-black w-full text-white py-1.5 px-4 rounded-md mt-5 signup-btn transition duration-200 ease-in-out hover:bg-gray-800">
                   Sign Up
                 </button>
               </form>
